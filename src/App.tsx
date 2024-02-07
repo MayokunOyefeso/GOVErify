cc2021bc846b3068f7eb99e7b9c90c5f6d747375
Mayokun Oyefeso <mayokunoyefeso@gmail.com>
2024-02-07 00:15:12 -0600

    Add globe and improve styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import './App.css'
-import { Button, Flex } from 'antd';
+import { Button } from 'antd';
+import globe from "./images/globe.png"
 
 
 function App() {
 
   return (
-    <div class="image">
-      <div class="image-overlay">
-      <h1 class="landing-logo">GOVerify</h1>
-      <h2 class="welcome-message"> Streamlining the management, and storage of international student records at Fisk University’s Office of 
-        Global Initiatives, GOVerify ensures an efficient process and offers a seamless experience.</h2>
-        <Button style={{ marginTop: "20px", float: "right", marginRight: "50px"}}> Let's get started</Button>
+    <div className="image">
+      <div>
+      <img src={globe} style={{ width: "140px", marginTop: "30px", float: "right", marginRight: "40px"}} alt="Globe" />
       </div>
+
+      <div className="image-overlay">
+      <h1 className="landing-logo">GOVerify</h1>
+      <h2 className="welcome-message"> Streamlining the management, and storage of international student records at Fisk University’s Office of 
+        Global Initiatives, GOVerify ensures an efficient process and offers a seamless experience.</h2>
+      <Button style={{ marginTop: "20px", float: "right", marginRight: "50px"}}> Let's get started</Button>
+      </div>
+      
     </div>
       
   )
